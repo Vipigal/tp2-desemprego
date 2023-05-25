95c0b0bd42bba580027f4245769e3c0f0a03e5ba
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-05-25 20:28:49 -0300

    adicionado entrada/saida do problema

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 #define INF 0x3f3f3f3f
 
 using namespace std;
 
+void imprimeMap(const unordered_map<string, vector<string>> &map){
+		for (auto v : map){
+			cout<<v.first<<" ";
+			for (auto i : v.second){
+				cout<<i<<" ";
+			}
+			cout<<endl;
+		}
+}
+
 int main(int argc, char const *argv[]) {
-    // Your code here
+    int num_usuarios{0}, num_empregos{0}, num_qualificacoes{0};
+		unordered_map<string, vector<string>> tb_usuario_emprego;
+		string usuario, cargo;
+
+		cin>>num_usuarios>>num_empregos>>num_qualificacoes;
+		while(cin>>usuario>>cargo){
+			tb_usuario_emprego[usuario].push_back(cargo);
+		}
+		
+		imprimeMap(tb_usuario_emprego);
+
     return 0;
 }
